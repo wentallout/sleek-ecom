6152fce70bc0b9beee14d27542ff95e7b7f44afd
Nguyen Dang Khoa <wentallout@gmail.com>
2024-03-01 20:22:39 +0700

    fix broken image imports

--- src/lib/components/sections/footer/social-icons.svelte
+++ src/lib/components/sections/footer/social-icons.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 	import { SOCIAL_FB_URL, SOCIAL_INSTA_URL, SOCIAL_X_URL } from '$lib/constants';
+
+	import fb from '$lib/icons/social/fb.svg';
+	import insta from '$lib/icons/social/insta.svg';
+
+	import x from '$lib/icons/social/x.svg';
 </script>
 
 <div class="flex flex-row flex-wrap gap-4">
 	<a
 		class="aspect-square rounded-full border border-solid border-neutral-300 p-2"
 		href={SOCIAL_FB_URL}>
-		<img src="./src/lib/icons/social/fb.svg" alt="" />
+		<img src={fb} alt="facebook" />
 	</a>
 	<a
 		class="aspect-square rounded-full border border-solid border-neutral-300 p-2"
 		href={SOCIAL_INSTA_URL}>
-		<img src="./src/lib/icons/social/insta.svg" alt="" />
+		<img src={insta} alt="instagram" />
 	</a>
 	<a
 		class="aspect-square rounded-full border border-solid border-neutral-300 p-2"
 		href={SOCIAL_X_URL}>
-		<img src="./src/lib/icons/social/x.svg" alt="" />
+		<img src={x} alt="x" />
 	</a>
 </div>
--- src/lib/components/sections/home/get-the-look.svelte
+++ src/lib/components/sections/home/get-the-look.svelte
@@ -1,5 +1,9 @@
 <script>
 	import HeadingWithShowAll from '$lib/components/ui/heading/heading-with-show-all.svelte';
+
+	import look1 from '$lib/images/look1.png';
+	import look2 from '$lib/images/look2.jpg';
+	import look3 from '$lib/images/look3.jpg';
 </script>
 
 <section class="g-container mb-28">
@@ -7,13 +11,13 @@
 
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-3">
 		<div class="aspect-square">
-			<img class="h-full w-full object-cover" src="./src/lib/images/look1.png" alt="" />
+			<img class="h-full w-full object-cover" src={look1} alt="" />
 		</div>
 		<div class="aspect-square">
-			<img class="h-full w-full object-cover" src="./src/lib/images/look2.jpg" alt="" />
+			<img class="h-full w-full object-cover" src={look2} alt="" />
 		</div>
 		<div class="aspect-square">
-			<img class="h-full w-full object-cover" src="./src/lib/images/look3.jpg" alt="" />
+			<img class="h-full w-full object-cover" src={look3} alt="" />
 		</div>
 	</div>
 </section>
