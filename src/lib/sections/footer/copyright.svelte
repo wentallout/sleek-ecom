4bcbec4c2cd77e54a4d10f324af1ceee275e20e2
Nguyen Dang Khoa <wentallout@gmail.com>
2024-03-05 13:08:30 +0700

    update some packages

--- src/lib/sections/footer/copyright.svelte
+++ src/lib/sections/footer/copyright.svelte
@@ -1,10 +1,10 @@
-<div class="border-t-black-500 w-full py-6">
+<div class="border-t-black-500 w-full border-solid py-6">
 	<div class="container flex flex-row flex-wrap items-center justify-between">
 		<div class="">© 2024 Nguyen Dang Khoa @wentallout</div>
 		<div class="flex flex-row gap-4">
-			<a href="#" class="underline">Privacy Policy</a>
-			<a href="#" class="underline">Terms & Conditions</a>
-			<a href="#" class="underline">Accessibility</a>
+			<a href="/" class="underline">Privacy Policy</a>
+			<a href="/" class="underline">Terms & Conditions</a>
+			<a href="/" class="underline">Accessibility</a>
 		</div>
 	</div>
 </div>
